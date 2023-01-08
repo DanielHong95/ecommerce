8f42bfea0abc09de587b0be6c6f510add8a80142
DanielHong95 <danielhong95@outlook.com>
2023-01-08 10:54:03 -0600

    filtering favorites

--- client/src/components/favoritescard/favoritescard.js
+++ client/src/components/favoritescard/favoritescard.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 function FavoritesCard({ id, category, category_id }) {
   const [favorites, setFavorites] = useState([]);
-  const [data, setData] = useState([]);
 
   console.log(category, category_id);
   // can't combine card and items components into one file because useEffect from items component prevents category and category_id data from being passed
 
   // delete favorites
-  const deleteFavorites = async (id) => {
+  const deleteFavorites = async () => {
     try {
       const response = await axios.delete(
         `http://localhost:5000/favorites/${id}`,
         {}
       );
       setFavorites(favorites.filter((favorite) => favorite.id !== id));
+      // filter not rendering out deleted favorite item
       console.log("favorite deleted");
     } catch (error) {
       console.log(error.message);
@@ -31,20 +31,25 @@ function FavoritesCard({ id, category, category_id }) {
         category: category,
         category_id: category_id,
       });
-      setData(request.data);
+      setFavorites(request.data);
       console.log("added to cart");
     } catch (error) {
       console.log(error.message);
     }
   };
 
+  // useEffect(() => {
+  //   setFavorites();
+  // });
+
   return (
-    <div key={id}>
+    <div>
       <div>
         {id}, {category}, {category_id}
       </div>
       <button onClick={addToCart}>Add to Cart</button>
-      <div onClick={() => deleteFavorites(id)}>
+      <div key={id} onClick={() => deleteFavorites(id)}>
+        {console.log(id)}
         <DeleteIcon />
       </div>
     </div>
--- client/src/pages/favoritespage/favoritespage.js
+++ client/src/pages/favoritespage/favoritespage.js
@@ -1,13 +1,10 @@
 import React from "react";
 import FavoritesItems from "../../components/favoritesitems/favoritesitems";
-import FavoritesTest from "../../components/favoritestest/favoritestest";
-import FavoritesCard from "../../components/favoritescard/favoritescard";
 
 function FavoritesPage() {
   return (
     <div>
       <div>Favorites</div>
-      {/* <FavoritesTest /> */}
       <FavoritesItems />
     </div>
   );
